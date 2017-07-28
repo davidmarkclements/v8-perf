f79a270e89d8a5eb296f338ac5f950e40d7ec251
Timothy Gu <timothygu99@gmail.com>
2017-07-28 22:34:25 +0800

    Fix benchmarks

--- bench/object-creation.js
+++ bench/object-creation.js
@@ -14,17 +14,18 @@ function MyCtor (x) {
   this.x = x
 }
 
+var obj
 
 suite.add('literal', function base () {
-  var obj = { x: 1 }
+  obj = { x: 1 }
 })
 
 suite.add('class', function allNums () {
-  var obj = new MyClass(1)
+  obj = new MyClass(1)
 })
 
 suite.add('constructor', function allNums () {
-  var obj = new MyCtor(1)
+  obj = new MyCtor(1)
 })
 
 suite.on('cycle', () => runs = 0)
