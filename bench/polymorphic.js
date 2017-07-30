09b4c10fd4042c054feb33fc30349ea8b44c3dde
Matteo Collina <hello@matteocollina.com>
2017-07-30 13:21:58 +0100

    Updated polymorphic bench and added new images

--- bench/polymorphic.js
+++ bench/polymorphic.js
@@ -4,118 +4,25 @@ var benchmark = require('benchmark')
 var suite = new benchmark.Suite()
 var runs = 0
 
-;(function () {
-  var runs = 0
-
-  suite.add('polymorphic with literal', function polymorphicLiteral () {
-    var arg
-    runs = (runs + 1) % 2147483647
-    if (runs % 2 === 0) {
-      arg = { length: 2 }
-    } else {
-      arg = "42"
-    }
-    multiply(arg)
-  })
-
-  function multiply (obj) {
-    var max = obj.length * 10
-    for (var i = 0; i < max; i++) {}
-    return i
+suite.add('polymorphic', function polymorphic() {
+  var objects = [{a:1}, {b:1, a:2}, {c:1, b:2, a:3}, {d:1, c:2, b:3, a:4}];
+  var sum = 0;
+  for (var i = 0; i < 10000; i++) {
+    var o = objects[i & 3];
+    sum += o.a;
   }
-})()
+  return sum;
+})
 
-;(function () {
-  var runs = 0
-
-  function A (length) {
-    this.length = length
+suite.add('monomorphic', function monomorphic() {
+  var objects = [{a:1}, {a:2}, {a:3}, {a:4}];
+  var sum = 0;
+  for (var i = 0; i < 10000; i++) {
+    var o = objects[i & 3];
+    sum += o.a;
   }
-
-  suite.add('polymorphic with constructor', function polymorphicConstructor () {
-    var arg
-    runs = (runs + 1) % 2147483647
-    if (runs % 2 === 0) {
-      arg = new A(2)
-    } else {
-      arg = "42"
-    }
-    multiply(arg)
-  })
-
-  function multiply (obj) {
-    var max = obj.length * 10
-    for (var i = 0; i < max; i++) {}
-    return i
-  }
-})()
-
-;(function () {
-  var runs = 0
-
-  suite.add('monomorphic string', function monomorphicString () {
-    var arg
-    runs = (runs + 1) % 2147483647
-    if (runs % 2 === 0) {
-      arg = "42"
-    } else {
-      arg = "42"
-    }
-    multiply(arg)
-  })
-
-  function multiply (obj) {
-    var max = obj.length * 10
-    for (var i = 0; i < max; i++) {}
-    return i
-  }
-})()
-
-;(function () {
-  var runs = 0
-
-  suite.add('monomorphic obj literal', function monomorphicLiteral () {
-    var arg
-    runs = (runs + 1) % 2147483647
-    if (runs % 2 === 0) {
-      arg = { length: 2 }
-    } else {
-      arg = { length: 2 }
-    }
-    multiply(arg)
-  })
-
-  function multiply (obj) {
-    var max = obj.length * 10
-    for (var i = 0; i < max; i++) {}
-    return i
-  }
-})()
-
-;(function () {
-  var runs = 0
-
-  function A (length) {
-    this.length = length
-  }
-
-  suite.add('monomorphic obj with constructor', () => {
-    var arg
-    runs = (runs + 1) % 2147483647
-    if (runs % 2 === 0) {
-      arg = new A(2)
-    } else {
-      arg = new A(2)
-    }
-    multiply(arg)
-  })
-
-  function multiply (obj) {
-    var max = obj.length * 10
-    for (var i = 0; i < max; i++) {}
-    return i
-  }
-})()
+  return sum;
+})
 
 suite.on('complete', require('./print'))
 
