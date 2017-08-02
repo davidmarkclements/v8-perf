b513648e88cd3927022f57da5eda797e3bc91464
Matteo Collina <hello@matteocollina.com>
2017-08-02 15:47:03 +0200

    Revert "include polymorphic types"
    This reverts commit febc72e11bf51f431378fea673846d4a56d6de58.

--- bench/polymorphic.js
+++ bench/polymorphic.js
@@ -5,40 +5,23 @@ var suite = new benchmark.Suite()
 var runs = 0
 
 suite.add('polymorphic', function polymorphic() {
-  var objects = [{a:1}, {b:1, a:2}, {c:1, b:2, a:3}, {d:1, c:2, b:3, a:4}]
-  var coercionBalancers = [undefined, 1, '1', true]
-  var sum = 0
+  var objects = [{a:1}, {b:1, a:2}, {c:1, b:2, a:3}, {d:1, c:2, b:3, a:4}];
+  var sum = 0;
   for (var i = 0; i < 10000; i++) {
-    var o = objects[i & 3]
-    sum += typeof o === 'object' ? o.a : o
-    if (i & 3) +coercionBalancers[i & 3]
+    var o = objects[i & 3];
+    sum += o.a;
   }
-  return sum
+  return sum;
 })
-
-suite.add('polymorphic (types)', function polymorphicTypes() {
-  var objects = [{a:1}, 1, '1', true]
-  var coercionBalancers = [undefined, 1, '1', true]
-  var sum = 0
-  for (var i = 0; i < 10000; i++) {
-    var o = objects[i & 3]
-    sum += typeof o === 'object' ? o.a : o
-    if (i & 3) coercionBalancers[i & 3] //lookup without coercion
-  }
-  return sum
-})
-
 
 suite.add('monomorphic', function monomorphic() {
-  var objects = [{a:1}, {a:2}, {a:3}, {a:4}]
-  var coercionBalancers = [undefined, 1, '1', true]
-  var sum = 0
+  var objects = [{a:1}, {a:2}, {a:3}, {a:4}];
+  var sum = 0;
   for (var i = 0; i < 10000; i++) {
-    var o = objects[i & 3]
-    sum += typeof o === 'object' ? o.a : +o
-    if (i & 3) +coercionBalancers[i & 3]
+    var o = objects[i & 3];
+    sum += o.a;
   }
-  return sum
+  return sum;
 })
 
 suite.on('complete', require('./print'))
